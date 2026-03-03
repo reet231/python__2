671634abb299534e23f8b985373be924f73a2d07
Dilpreet lubana <dilpreetkaurlubana29@gmail.com>
2026-03-03 19:00:47 +0530

    Add files via upload

---
+++ Pra 6 Assignment 2.py
@@ -0,0 +1,11 @@
+"""Design a function that accepts sequence of lines as input and prints the lines after making all characters in the sentence capitalized. Suppos Suppose the following llowing input input is supplied to to the the program:
+
+Practice makes perfect
+
+Then, the output should be: PRACTICE MAKES PERFECT"""
+
+
+text = input("Enter a sentence: ")
+
+result = text.upper()
+print("Output:", result)
